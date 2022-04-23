df968f4bf9cce02edd77674fbc69a1c6ba473326
taemini <tamini.kim@gmail.com>
2022-04-24 00:56:19 +0900

    feat: create own *_FindModule

---
+++ section9/GTK_FindModule/cmake/modules/FindGTK3.cmake
@@ -0,0 +1,14 @@
+find_library(GTK3_LIBRARY NAMES gtk-3)
+find_path(GTK3_INCLUDE_DIR NAMES gtk/gtk.h PATH_SUFFIXES gtk-3.0)
+find_path(GLIB_INCLUDE_DIR NAMES glib.h PATH_SUFFIXES glib-2.0)
+
+# default (.../include) 폴더에 존재하지 않아  HINTS 제공
+find_path(GLIBCONFIG_INCLUDE_DIR NAMES glibconfig.h HINTS /usr/local/Cellar/glib/2.70.2/lib PATH_SUFFIXES glib-2.0/include)
+find_path(PANGO_INCLUDE_DIR NAMES pango/pango.h PATH_SUFFIXES pango-1.0)
+find_path(HB_INCLUDE_DIR NAMES hb.h PATH_SUFFIXES harfbuzz)
+find_path(CAIRO_INCLUDE_DIR NAMES cairo.h PATH_SUFFIXES cairo)
+find_path(GDKPIXBUF_INCLUDE_DIR NAMES gdk-pixbuf/gdk-pixbuf.h PATH_SUFFIXES gdk-pixbuf-2.0)
+find_path(ATK_INCLUDE_DIR NAMES atk/atk.h PATH_SUFFIXES atk-1.0)
+
+set(GTK3_INCLUDE_DIRS ${GTK3_INCLUDE_DIR} ${GLIB_INCLUDE_DIR} ${GLIBCONFIG_INCLUDE_DIR} ${PANGO_INCLUDE_DIR} ${HB_INCLUDE_DIR} ${CAIRO_INCLUDE_DIR} ${GDKPIXBUF_INCLUDE_DIR} ${ATK_INCLUDE_DIR})
+set(GTK3_LIBRARIES ${GTK3_LIBRARY})
